c12926610744476bbcaff3512c0955f89cf0a5ab
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-11-15 10:18:16 -0600

    progress

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { serveStatic } from "@hono/node-server/serve-static";
 import { ssr as helloWorldSSR } from "./public/hello-world-html.js";
 import { ssr as radioGroupSSR } from "./public/radio-group-html.js";
 
+const html = String.raw;
 const app = new Hono();
 
 app.use("/*", serveStatic({ root: "./public" }));
@@ -14,6 +15,27 @@ app.get("/hello-world", (c) => {
     <div>The following content was server-side rendered:</div>
     ${helloWorldSSR()}
     ${helloWorldSSR(name)}
+  `);
+});
+
+// This returns a new page of HTML that includes
+// server-side rendered web components.
+app.get("/new-page", (c) => {
+  return c.html(html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>New Page</title>
+        <script type="module">
+          import "./hello-world.js";
+        </script>
+      </head>
+      <body>
+        <p>The following are server-side rendered web components.</p>
+        <hello-world></hello-world>
+        <hello-world name="SSR"></hello-world>
+      </body>
+    </html>
   `);
 });
 
